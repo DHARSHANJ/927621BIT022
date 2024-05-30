bb7f20a5b68614c7e0fa01898b443dea90c7edc1
Dharshan J <117148127+DHARSHANJ@users.noreply.github.com>
2024-05-30 12:39:23 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+// Footer component
+const Footer = () => {
+  return (
+    <footer className="buybetter-footer">
+      &copy; 2024 Employee Management System. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
